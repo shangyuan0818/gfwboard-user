2629f45a97b7727c2c7dc1e86709a1e8216187aa
AH-dark <ahdark@outlook.com>
2022-12-30 16:23:34 +0800

    Revert split configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,6 @@ import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import image from "@rollup/plugin-image";
 
-const manualChunks: Record<string, string[]> = {
-  react: ["react", "react-dom"],
-  router: ["react-router", "react-router-dom"],
-  "mui-base": ["@emotion", "@mui/base", "@mui/system"],
-  mui: ["@mui/material"],
-  "mui-plugin": ["@mui/lab", "@mui/x-date-pickers", "tss-react", "@mui/icons-material"],
-  "mui-third-party": ["mui-markdown", "markdown-to-jsx", "notistack"],
-  carousel: ["react-material-ui-carousel", "framer-motion"],
-  ant: ["@ant-design"],
-  ahooks: ["ahooks", "@ahooksjs"],
-  axios: ["axios"],
-  lodash: ["lodash-es"],
-  redux: ["redux", "react-redux", "@reduxjs/toolkit"],
-  formik: ["formik", "yup"],
-  i18n: ["i18next", "react-i18next", "i18next-browser-languagedetector"],
-  prism: ["prism-react-renderer"],
-  polyfill: ["core-js", "resize-observer-polyfill"]
-};
-
 /**
  * @type {import('vite').UserConfig}
  * @see https://vitejs.dev/config/
@@ -42,28 +23,12 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "static",
     cssCodeSplit: true,
-    chunkSizeWarningLimit: 300,
+    chunkSizeWarningLimit: 500,
+    modulePreload: {
+      polyfill: true
+    },
     rollupOptions: {
       output: {
-        manualChunks: (meta) => {
-          if (meta.includes("node_modules")) {
-            for (const [name, pkgs] of Object.entries(manualChunks)) {
-              for (const pkg of pkgs) {
-                if (meta.includes(`/node_modules/${pkg}/`)) {
-                  return name;
-                }
-              }
-            }
-
-            return "vendor";
-          }
-
-          if (meta.includes("src")) {
-            return "app";
-          }
-
-          return "shared";
-        },
         chunkFileNames: "static/js/[name].[hash:12].chunk.js",
         entryFileNames: "static/js/[name].[hash:12].js",
         assetFileNames: (info) => {

03bef51a18b4e5aa1d69394d4e9f10f396787b0c
AH-dark <ahdark@outlook.com>
2023-01-01 15:37:09 +0800

    Chore: code formatted for `useKnowledge` hook

--- src/hooks/useKnowledge.ts
+++ src/hooks/useKnowledge.ts
@@ -1,21 +1,23 @@
+import { useMemo } from "react";
+
+// third-party
+import lo from "lodash-es";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
-import { useMemo } from "react";
+
+// project imports
 import { useGetKnowledgeQuery } from "@/store/services/api";
-import lo from "lodash-es";
 
 const useKnowledge = () => {
   const { i18n } = useTranslation();
   const params = useParams<{ id: string }>();
   const postId = useMemo(() => (!lo.isEmpty(params.id) && params.id ? parseInt(params.id) : null), [params.id]);
-  const knowledgeQuery = useGetKnowledgeQuery(
+  return useGetKnowledgeQuery(
     { id: postId!, language: i18n.language },
     {
       skip: !postId
     }
   );
-
-  return knowledgeQuery;
 };
 
 export default useKnowledge;

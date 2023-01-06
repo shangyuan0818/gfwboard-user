35872fe09f0947a887da3c684ea264c4dd8b1532
AH-dark <ahdark@outlook.com>
2023-01-07 01:58:02 +0800

    Feat: error handle for checkout page

--- src/sections/order/checkoutPage/context.ts
+++ src/sections/order/checkoutPage/context.ts
@@ -4,31 +4,50 @@ import constate from "constate";
 // project imports
 import { useCheckOrderQuery, useGetOrderDetailQuery } from "@/store/services/api";
 import { OrderStatus } from "@/model/order";
+import { useTranslation } from "react-i18next";
+import { useSnackbar } from "notistack";
 
 export interface CheckoutPageContext {
   tradeNo: string;
 }
 
 const useContext = ({ tradeNo }: CheckoutPageContext) => {
+  const { t } = useTranslation();
+  const { enqueueSnackbar } = useSnackbar();
+
   const [status, setStatus] = useState<OrderStatus>(OrderStatus.PENDING);
 
   const detailQuery = useGetOrderDetailQuery(tradeNo);
   const callbackQuery = useCheckOrderQuery(tradeNo, {
-    skip: status !== OrderStatus.PENDING,
+    skip: status !== OrderStatus.PENDING || !detailQuery.isSuccess,
     pollingInterval: 1000
   });
 
+  // check order status
   useEffect(() => {
     if (detailQuery.data?.status) {
       setStatus(detailQuery.data.status);
     }
   }, [detailQuery.data?.status]);
 
+  // sync status from callback
   useEffect(() => {
     if (callbackQuery.data) {
       setStatus(callbackQuery.data);
     }
   }, [callbackQuery.data]);
+
+  // check if order error
+  useEffect(() => {
+    if (detailQuery.error) {
+      enqueueSnackbar(
+        t("notice::order-detail-error", {
+          error: detailQuery.error.message
+        }),
+        { variant: "error" }
+      );
+    }
+  }, [detailQuery.error]);
 
   return {
     tradeNo,

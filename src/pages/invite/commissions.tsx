ce6b5184520ca1249da3ac11f5cb0f9bbcffd809
AH-dark <ahdark@outlook.com>
2023-01-13 01:30:42 +0800

    Feat: commissions list page

--- src/pages/invite/commissions.tsx
+++ src/pages/invite/commissions.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import useTitle from "@/hooks/useTitle";
+import CommissionsPage from "@/sections/invite/commissionsPage";
 
 const Commissions: React.FC = () => {
   useTitle("invite-commissions");
 
-  return <></>;
+  return <CommissionsPage />;
 };
 
 export default Commissions;

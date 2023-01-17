fdcce1939755792c3d428870bbd9f9f9a2110e64
AH-dark <ahdark@outlook.com>
2023-01-18 02:35:39 +0800

    Chore: use different measurement id in different domains

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,21 @@ export const drawerWidth = 260;
 
 // ==============================|| THEME CONFIG  ||============================== //
 
+const getGA = () => {
+  if (import.meta.env.DEV) {
+    return "";
+  }
+
+  switch (window.location.hostname) {
+    case "abc.gfwnetwork.com":
+      return "G-HSJEER5NF4";
+    case "beta.v2b.rip":
+      return "G-D6GDYEWSQT";
+    default:
+      return "";
+  }
+};
+
 const config: DefaultConfigProps = {
   defaultPath: "/",
   fontFamily: `'Public Sans', sans-serif`,
@@ -20,7 +35,7 @@ const config: DefaultConfigProps = {
   api: "https://sub.crosswalltech.com",
   languages: ["zh-CN", "en-US"],
   googleAnalytics: {
-    measurementId: "G-D6GDYEWSQT"
+    measurementId: getGA()
   },
   emojiEndpoint: "https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/{{code}}.png"
 };

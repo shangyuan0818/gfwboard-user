1eb4202dd5394063c9156b01546e4a3391fc9175
AH-dark <ahdark@outlook.com>
2023-01-21 22:26:03 +0800

    Chore: use tss-react instead of `sx` props

--- src/sections/auth/AuthWrapper.tsx
+++ src/sections/auth/AuthWrapper.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 
 // material-ui
 import { Box, Grid } from "@mui/material";
@@ -7,6 +7,7 @@ import { Box, Grid } from "@mui/material";
 import AuthFooter from "@/components/cards/AuthFooter";
 import Logo from "@/components/logo";
 import AuthCard from "./AuthCard";
+import { makeStyles } from "@/themes/hooks";
 
 // assets
 import AuthBackground from "@/assets/images/auth/AuthBackground";
@@ -15,41 +16,53 @@ interface Props {
   children: ReactNode;
 }
 
+const useStyles = makeStyles({
+  name: "AuthWrapper"
+})((theme) => ({
+  root: { minHeight: "100vh" },
+  logoGrid: {
+    marginLeft: theme.spacing(3),
+    marginTop: theme.spacing(3)
+  },
+  authCardGrid: {
+    minHeight: "calc(100vh - 210px)",
+    [theme.breakpoints.up("sm")]: {
+      minHeight: "calc(100vh - 134px)"
+    },
+    [theme.breakpoints.up("md")]: {
+      minHeight: "calc(100vh - 112px)"
+    }
+  },
+  footerGrid: {
+    margin: theme.spacing(1, 3, 3, 3)
+  }
+}));
+
 // ==============================|| AUTHENTICATION - WRAPPER ||============================== //
 
-const AuthWrapper = ({ children }: Props) => (
-  <Box sx={{ minHeight: "100vh" }}>
-    <AuthBackground />
-    <Grid
-      container
-      direction="column"
-      justifyContent="flex-end"
-      sx={{
-        minHeight: "100vh"
-      }}
-    >
-      <Grid item xs={12} sx={{ ml: 3, mt: 3 }}>
-        <Logo />
-      </Grid>
-      <Grid item xs={12}>
-        <Grid
-          item
-          xs={12}
-          container
-          justifyContent="center"
-          alignItems="center"
-          sx={{ minHeight: { xs: "calc(100vh - 210px)", sm: "calc(100vh - 134px)", md: "calc(100vh - 112px)" } }}
-        >
-          <Grid item>
-            <AuthCard>{children}</AuthCard>
+const AuthWrapper: React.FC<Props> = ({ children }) => {
+  const { classes } = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <AuthBackground />
+      <Grid container direction="column" justifyContent="flex-end" className={classes.root}>
+        <Grid item xs={12} className={classes.logoGrid}>
+          <Logo />
+        </Grid>
+        <Grid item xs={12}>
+          <Grid item xs={12} container justifyContent="center" alignItems="center" className={classes.authCardGrid}>
+            <Grid item>
+              <AuthCard>{children}</AuthCard>
+            </Grid>
           </Grid>
         </Grid>
+        <Grid item xs={12} className={classes.footerGrid}>
+          <AuthFooter />
+        </Grid>
       </Grid>
-      <Grid item xs={12} sx={{ m: 3, mt: 1 }}>
-        <AuthFooter />
-      </Grid>
-    </Grid>
-  </Box>
-);
+    </Box>
+  );
+};
 
 export default AuthWrapper;
